03d023812d9dd96bee70c76fcf0484c1269db608
Alahgon <alejandrolahiguera8@gmail.com>
2026-06-09 20:25:17 +0200

    Animación de personaje

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -3,25 +3,76 @@ using UnityEngine;
 public class PlayerMovement2D : MonoBehaviour
 {
     public float speed = 5f;
-
     public Rigidbody2D rb;
 
+    [Header("Configuración de Lag Retró")]
+    [Tooltip("Cuánto tiempo (en segundos) tarda el personaje en detenerse tras soltar las teclas")]
+    public float inputLagDuration = 0.12f;
+    private float lagTimer;
+
     private Vector2 moveInput;
+    private Animator animator;
+    private bool is_Moving;
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+        is_Moving = false;
+    }
 
     void Update()
     {
-     
-        moveInput.x = Input.GetAxisRaw("Horizontal");
-        moveInput.y = Input.GetAxisRaw("Vertical");
+      
+        float rawX = Input.GetAxisRaw("Horizontal");
+        float rawY = Input.GetAxisRaw("Vertical");
+
+       
+        if (rawX != 0) rawY = 0;
+
+      
+        if (rawX != 0 || rawY != 0)
+        {
+           
+            moveInput.x = rawX;
+            moveInput.y = rawY;
+            is_Moving = true;
+
+            lagTimer = inputLagDuration;
+        }
+        else
+        {
+           
+            if (lagTimer > 0)
+            {
+                lagTimer -= Time.deltaTime;
+                
+            }
+            else
+            {
+                
+                moveInput = Vector2.zero;
+                is_Moving = false;
+            }
+        }
+
+        if (is_Moving)
+        {
+            animator.SetFloat("Horizontal", moveInput.x);
+            animator.SetFloat("Vertical", moveInput.y);
+        }
+
+        animator.SetBool("Is_Moving", is_Moving);
     }
 
     void FixedUpdate()
     {
-
-        if (!Scene2ManagerScript.instance._moveIsPaused)
+        if (Scene2ManagerScript.instance != null && !Scene2ManagerScript.instance._moveIsPaused)
         {
-            rb.linearVelocity = new Vector2(moveInput.x * speed, moveInput.y * speed);
+            rb.linearVelocity = moveInput * speed;
         }
-        
+        else if (Scene2ManagerScript.instance != null && Scene2ManagerScript.instance._moveIsPaused)
+        {
+            rb.linearVelocity = Vector2.zero;
+        }
     }
 }
